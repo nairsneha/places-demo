2dd6bb4a341e9a05aba052907a9745cf5d1109ba
nairsneha <nair.sneha1997@gmail.com>
2022-05-02 16:04:11 -0400

    starting places

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    avocado: 0
+  }
+  render() {
+    return(
+        <div>
+          <h1>{this.state.avocado}</h1>
+          {this.state.avocado % 6 === 0 &&
+           <button onClick={() => this.setState((mango) => ({
+
+             avocado: ++mango.avocado}))}>
+             CHART
+           </button>}
+          {this.state.avocado % 4 !== 0 &&
+           <button onClick={() => this.setState((strawberry) => ({avocado: --strawberry.avocado}))}>
+             EMPLOYEE
+           </button>}
+        </div>
+    )
+  }
 }
 
 export default App;
